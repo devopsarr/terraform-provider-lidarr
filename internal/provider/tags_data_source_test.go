e49848680604b72d1badba44e1379a5b04d081a3
Fuochi <ffuochi@hotmail.com>
2022-08-26 09:24:48 +0400

    test: set parallel tests

--- internal/provider/tags_data_source_test.go
+++ internal/provider/tags_data_source_test.go
@@ -7,19 +7,21 @@ import (
 )
 
 func TestAccTagsDataSource(t *testing.T) {
+	t.Parallel()
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create a tag to have a value to check
 			{
-				Config: testAccTagResourceConfig("test-1", "hvec") + testAccTagResourceConfig("test-2", "mp3"),
+				Config: testAccTagResourceConfig("test-1", "spotify") + testAccTagResourceConfig("test-2", "deezer"),
 			},
 			// Read testing
 			{
 				Config: testAccTagsDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckTypeSetElemNestedAttrs("data.lidarr_tags.test", "tags.*", map[string]string{"label": "mp3"}),
+					resource.TestCheckTypeSetElemNestedAttrs("data.lidarr_tags.test", "tags.*", map[string]string{"label": "spotify"}),
 				),
 			},
 		},
