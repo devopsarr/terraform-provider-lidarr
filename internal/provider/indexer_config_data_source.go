001ef8aa2d97f20563f65a0165f5faa7b06f6c45
Fuochi <ffuochi@hotmail.com>
2022-11-24 09:16:25 +0400

    feat: add indexer config datasource

---
+++ internal/provider/indexer_config_data_source.go
@@ -0,0 +1,101 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/devopsarr/terraform-provider-sonarr/tools"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"golift.io/starr/lidarr"
+)
+
+const indexerConfigDataSourceName = "indexer_config"
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &IndexerConfigDataSource{}
+
+func NewIndexerConfigDataSource() datasource.DataSource {
+	return &IndexerConfigDataSource{}
+}
+
+// IndexerConfigDataSource defines the indexer config implementation.
+type IndexerConfigDataSource struct {
+	client *lidarr.Lidarr
+}
+
+func (d *IndexerConfigDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + indexerConfigDataSourceName
+}
+
+func (d *IndexerConfigDataSource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		// This description is used by the documentation generator and the delay server.
+		MarkdownDescription: "<!-- subcategory:Indexers -->[Indexer Config](../resources/indexer_config).",
+		Attributes: map[string]tfsdk.Attribute{
+			"id": {
+				MarkdownDescription: "Delay Profile ID.",
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"maximum_size": {
+				MarkdownDescription: "Maximum size.",
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"minimum_age": {
+				MarkdownDescription: "Minimum age.",
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"retention": {
+				MarkdownDescription: "Retention.",
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"rss_sync_interval": {
+				MarkdownDescription: "RSS sync interval.",
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+		},
+	}, nil
+}
+
+func (d *IndexerConfigDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*lidarr.Lidarr)
+	if !ok {
+		resp.Diagnostics.AddError(
+			tools.UnexpectedDataSourceConfigureType,
+			fmt.Sprintf("Expected *lidarr.Lidarr, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+func (d *IndexerConfigDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	// Get indexer config current value
+	response, err := d.client.GetIndexerConfigContext(ctx)
+	if err != nil {
+		resp.Diagnostics.AddError(tools.ClientError, fmt.Sprintf("Unable to read %s, got error: %s", indexerConfigDataSourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "read "+indexerConfigDataSourceName)
+
+	status := IndexerConfig{}
+	status.write(response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, status)...)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -149,6 +149,7 @@ func (p *LidarrProvider) Resources(ctx context.Context) []func() resource.Resour
 
 func (p *LidarrProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
+		NewIndexerConfigDataSource,
 		NewIndexerDataSource,
 		NewIndexersDataSource,
 		NewDownloadClientConfigDataSource,
