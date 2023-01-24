7e6087aba687bef5eb2773d7e36faec4fc59aac6
Fuochi <ffuochi@hotmail.com>
2023-01-24 15:05:01 +0400

    feat: add root folder resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -169,6 +169,7 @@ func (p *LidarrProvider) Resources(ctx context.Context) []func() resource.Resour
 		NewIndexerConfigResource,
 
 		// Media Management
+		NewRootFolderResource,
 
 		// Notifications
 		NewNotificationResource,
