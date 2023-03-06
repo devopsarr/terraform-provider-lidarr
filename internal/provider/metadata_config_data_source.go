f4303ed8215f437281f002485ee5e276bcb8c004
Fuochi <ffuochi@hotmail.com>
2023-03-06 17:43:02 +0400

    feat: add metadata config data source

---
+++ internal/provider/metadata_config_data_source.go
@@ -0,0 +1,76 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/devopsarr/lidarr-go/lidarr"
+	"github.com/devopsarr/terraform-provider-lidarr/internal/helpers"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+const metadataConfigDataSourceName = "metadata_config"
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &MetadataConfigDataSource{}
+
+func NewMetadataConfigDataSource() datasource.DataSource {
+	return &MetadataConfigDataSource{}
+}
+
+// MetadataConfigDataSource defines the metadata config implementation.
+type MetadataConfigDataSource struct {
+	client *lidarr.APIClient
+}
+
+func (d *MetadataConfigDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + metadataConfigDataSourceName
+}
+
+func (d *MetadataConfigDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the delay server.
+		MarkdownDescription: "<!-- subcategory:Metadata -->[Metadata Config](../resources/metadata_config).",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.Int64Attribute{
+				MarkdownDescription: "Metadata Config ID.",
+				Computed:            true,
+			},
+			"write_audio_tags": schema.StringAttribute{
+				MarkdownDescription: "Write audio tags.",
+				Computed:            true,
+			},
+			"metadata_source": schema.StringAttribute{
+				MarkdownDescription: "Metadata Source.",
+				Computed:            true,
+			},
+			"scrub_audio_tags": schema.BoolAttribute{
+				MarkdownDescription: "Scrub audio tags.",
+				Computed:            true,
+			},
+		},
+	}
+}
+
+func (d *MetadataConfigDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if client := helpers.DataSourceConfigure(ctx, req, resp); client != nil {
+		d.client = client
+	}
+}
+
+func (d *MetadataConfigDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	// Get metadata config current value
+	response, _, err := d.client.MetadataProviderConfigApi.GetMetadataProviderConfig(ctx).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(helpers.ClientError, helpers.ParseClientError(helpers.Read, metadataConfigDataSourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "read "+metadataConfigDataSourceName)
+
+	status := MetadataConfig{}
+	status.write(response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, status)...)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -258,6 +258,9 @@ func (p *LidarrProvider) DataSources(ctx context.Context) []func() datasource.Da
 		NewRootFolderDataSource,
 		NewRootFoldersDataSource,
 
+		// Metadata
+		NewMetadataConfigDataSource,
+
 		// Notifications
 		NewNotificationDataSource,
 		NewNotificationsDataSource,
