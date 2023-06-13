3bd6db08e6180435ba48aabe0e9f6413425a3487
Fuochi <ffuochi@hotmail.com>
2023-06-13 11:35:26 +0400

    feat: remove obsolete indexer rarbg

--- examples/resources/lidarr_indexer_rarbg/resource.tf
+++
@@ -1,7 +0,0 @@
-resource "lidarr_indexer_rargb" "example" {
-  enable_automatic_search = true
-  name                    = "Example"
-  base_url                = "https://torrentapi.org"
-  ranked_only             = false
-  minimum_seeders         = 1
-}
